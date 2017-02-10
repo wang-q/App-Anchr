ae989a441e2856766507d1aa26787ca46b92298a
wang-q <wang-q@outlook.com>
2017-02-10 20:57:27 +0800

    `brew list` is faster than `brew install`

--- share/install_dep.sh
+++ share/install_dep.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
+check_install () {
+    if brew list --versions $1 > /dev/null; then
+        echo "$1 already installed"
+    else
+        brew install $1;
+    fi
+}
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    brew install jdk
+    check_install jdk
 fi
 
 for package in parallel pigz;
 do
-    brew install ${package};
-    [ $? -ne 0 ] && exit 1;
+    check_install ${package}
 done
 
-for package in bbtools bedtools picard-tools samtools sickle;
+for package in bbtools bedtools fastqc picard-tools samtools sickle;
 do
-    brew install homebrew/science/${package};
-    [ $? -ne 0 ] && exit 1;
+    check_install homebrew/science/${package};
 done
 
-for package in faops jrunlist sparsemem dazz_db@20161112 daligner@20170203 scythe;
+for package in faops jrunlist scythe sparsemem dazz_db@20161112 daligner@20170203;
 do
-    brew install wang-q/tap/${package};
-    [ $? -ne 0 ] && exit 1;
+    check_install wang-q/tap/${package};
 done
 
 exit 0
