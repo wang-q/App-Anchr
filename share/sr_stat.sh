7bd02297a135b1031d4c1e483e34b4182010372f
wang-q <wang-q@outlook.com>
2017-02-11 10:57:53 +0800

    Merge stat 1 and 2

--- share/sr_stat.sh
+++ share/sr_stat.sh
@@ -44,81 +44,50 @@ GENOME_SIZE=$3
 #----------------------------#
 if [ "${STAT_TASK}" = "1" ]; then
     if [ "${RESULT_DIR}" = "header" ]; then
-        printf "| %s | %s | %s | %s | %s | %s | %s | %s | %s | \n" \
-            "Name" "fqSize" "faSize" "Length" "Kmer" "EstG" "SumSR" "SR/EstG" "RunTime"
-        printf "|:--|--:|--:|--:|--:|--:|--:|--:|--:|--:|\n"
+        printf "| %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | \n" \
+            "Name" \
+            "SumFq" "CovFq" "AvgRead" "Kmer" \
+            "SumFa" "Discard%" "#Subs" "Subs%" \
+            "RealG" "EstG" "Est/Real" \
+            "SumSR" "SumSR/Real" "SR/EstG" \
+            "RunTime"
+        printf "|:--|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|\n"
+    elif [ "${GENOME_SIZE}" -ne "${GENOME_SIZE}" ]; then
+        log_warn "Need a integer for GENOME_SIZE"
+        exit 1;
     elif [ -e "${RESULT_DIR}/environment.sh" ]; then
         log_debug "${RESULT_DIR}"
         cd "${RESULT_DIR}"
 
-        SECS=$(expr $(stat -c %Y super1.err) - $(stat -c %Y superreads.sh))
-        EST_G=$( cat environment.sh \
-            | perl -n -e '/ESTIMATED_GENOME_SIZE=\"(\d+)\"/ and print $1' )
+        SUM_FQ=$( if [ -e pe.renamed.fastq ]; then faops n50 -H -N 0 -S pe.renamed.fastq; else echo 0; fi )
+        SUM_FA=$( faops n50 -H -N 0 -S pe.cor.fa )
+        TOTAL_SUBS=$( cat pe.cor.fa | tr ' ' '\n' | grep ":sub:" | wc -l )
+        EST_G=$( cat environment.sh | perl -n -e '/ESTIMATED_GENOME_SIZE=\"(\d+)\"/ and print $1' )
         SUM_SR=$( faops n50 -H -N 0 -S work1/superReadSequences.fasta)
+        SECS=$(expr $(stat -c %Y super1.err) - $(stat -c %Y superreads.sh))
 
         printf "| %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | \n" \
             $( basename $( pwd ) ) \
-            $( if [[ -e pe.renamed.fastq ]]; then \
-                    du -h pe.renamed.fastq | cut -f1; \
-                else echo 0; \
-                fi ) \
-            $( du -h pe.cor.fa | cut -f1 ) \
-            $( cat environment.sh \
-                | perl -n -e '/PE_AVG_READ_LENGTH=\"(\d+)\"/ and print $1' ) \
-            $( cat environment.sh \
-                | perl -n -e '/KMER=\"(\d+)\"/ and print $1' ) \
-            ${EST_G} \
-            ${SUM_SR} \
-            $( perl -e "printf qq{%.2f}, ${SUM_SR} * 1.0 / ${EST_G}" )
-            $( printf "%d:%02d'%02d''\n" $((${SECS}/3600)) $((${SECS}%3600/60)) $((${SECS}%60)) ) \
-    else
-        log_warn "RESULT_DIR not exists"
-    fi
-
-elif [ "${STAT_TASK}" = "2" ]; then
-    if [ "${RESULT_DIR}" = "header" ]; then
-        printf "| %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | \n" \
-            "Name" \
-            "TotalFq" "TotalFa" "RatioDiscard" "TotalSubs" "RatioSubs" \
-            "RealG" "CovFq" "CovFa" \
-            "EstG" "SumSR" "Est/Real" "SumSR/Real" "N50SR"
-        printf "|:--|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|--:|\n"
-    elif [ "${GENOME_SIZE}" -ne "${GENOME_SIZE}" ]; then
-        log_warn "Need a integer for GENOME_SIZE"
-        exit 1;
-    elif [ -e "${RESULT_DIR}/work1/superReadSequences.fasta" ]; then
-        log_debug "${RESULT_DIR}"
-        cd "${RESULT_DIR}"
-
-        TOTAL_FQ=$( if [[ -e pe.renamed.fastq ]]; then \
-                faops n50 -H -N 0 -S pe.renamed.fastq; \
-            else echo 0; \
-            fi )
-        TOTAL_FA=$( faops n50 -H -N 0 -S pe.cor.fa )
-        EST_G=$( cat environment.sh \
-            | perl -n -e '/ESTIMATED_GENOME_SIZE=\"(\d+)\"/ and print $1' )
-        SUM_SR=$( faops n50 -H -N 0 -S work1/superReadSequences.fasta )
-        N50_SR=$( faops n50 -H -N 50 work1/superReadSequences.fasta )
-        TOTAL_SUBS=$( cat pe.cor.fa | tr ' ' '\n' | grep ":sub:" | wc -l )
-
-        printf "| %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | \n" \
-            $( basename $( pwd ) ) \
             \
-            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${TOTAL_FQ})") \
-            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${TOTAL_FA})") \
-            $( perl -e "printf qq{%.4f}, 1 - ${TOTAL_FA} / ${TOTAL_FQ}" ) \
-            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${TOTAL_SUBS})") \
-            $( perl -e "printf qq{%.4f}, ${TOTAL_SUBS} / ${TOTAL_FA}" ) \
+            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${SUM_FQ}, base => 1000,)") \
+            $( perl -e "printf qq{%.1f}, ${SUM_FQ} / ${GENOME_SIZE}" ) \
+            $( cat environment.sh | perl -n -e '/PE_AVG_READ_LENGTH=\"(\d+)\"/ and print $1' ) \
+            $( cat environment.sh | perl -n -e '/KMER=\"(\d+)\"/ and print $1' ) \
             \
-            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${GENOME_SIZE})") \
-            $( perl -e "printf qq{%.1f}, ${TOTAL_FQ} / ${GENOME_SIZE}" ) \
-            $( perl -e "printf qq{%.1f}, ${TOTAL_FA} / ${GENOME_SIZE}" ) \
+            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${SUM_FA}, base => 1000,)") \
+            $( perl -e "printf qq{%.2f%%}, 1 - ${SUM_FA} / ${SUM_FQ}" ) \
+            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${TOTAL_SUBS}, base => 1000,)") \
+            $( perl -e "printf qq{%.2f%%}, ${TOTAL_SUBS} / ${SUM_FA}" ) \
             \
-            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${EST_G})") \
-            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${SUM_SR})") \
+            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${GENOME_SIZE}, base => 1000,)") \
+            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${EST_G}, base => 1000,)") \
             $( perl -e "printf qq{%.2f}, ${EST_G} / ${GENOME_SIZE}" ) \
+            \
+            $( perl -MNumber::Format -e "print Number::Format::format_bytes(${SUM_SR}, base => 1000,)") \
+            $( perl -e "printf qq{%.2f}, ${SUM_SR} / ${EST_G}" )
             $( perl -e "printf qq{%.2f}, ${SUM_SR} / ${GENOME_SIZE}" ) \
-            ${N50_SR}
+            \
+            $( printf "%d:%02d'%02d''\n" $((${SECS}/3600)) $((${SECS}%3600/60)) $((${SECS}%60)) ) \
     else
         log_warn "RESULT_DIR not exists"
     fi
