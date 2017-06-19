f6c89c4b297d1a8efaf97402d63410f95cc3e397
wang-q <wang-q@outlook.com>
2017-06-19 10:41:32 +0800

    Use jrange to create covered.txt
    Much faster than original Perl version

--- lib/App/Anchr/Command/trimlong.pm
+++ lib/App/Anchr/Command/trimlong.pm
@@ -97,79 +97,22 @@ sub execute {
         }
     }
 
-    # seq_name => tier_of => { 1 => intspan, 2 => intspan}
-    my $covered = {};
+    {    # paf to covered
+        my $cmd;
+        $cmd .= "jrange covered";
+        $cmd .= " $basename.paf";
+        $cmd .= " --longest --paf";
+        $cmd .= " -o $basename.covered.txt";
+        App::Anchr::Common::exec_cmd( $cmd, { verbose => $opt->{verbose}, } );
 
-    {    # load overlaps and build coverages
-        my %seen_pair;
-
-        for my $line ( $tempdir->child("$basename.ovlp.tsv")->lines( { chomp => 1 } ) ) {
-            my $info = App::Anchr::Common::parse_ovlp_line($line);
-
-            # ignore self overlapping
-            next if $info->{f_id} eq $info->{g_id};
-
-            # ignore poor overlaps
-            next if $info->{ovlp_len} < $opt->{len};
-
-            # skip duplicated overlaps
-            my $pair = join( "\t", sort ( $info->{f_id}, $info->{g_id} ) );
-            next if $seen_pair{$pair};
-            $seen_pair{$pair}++;
-
-            {    # first seq
-                if ( !exists $covered->{ $info->{f_id} } ) {
-                    $covered->{ $info->{f_id} }
-                        = { all => AlignDB::IntSpan->new->add_pair( 1, $info->{f_len} ), };
-                    for my $i ( 1 .. $opt->{coverage} ) {
-                        $covered->{ $info->{f_id} }{$i} = AlignDB::IntSpan->new;
-                    }
-                }
-
-                my ( $beg, $end, ) = App::Anchr::Common::beg_end( $info->{f_B}, $info->{f_E}, );
-                App::Anchr::Common::bump_coverage( $covered->{ $info->{f_id} },
-                    $beg, $end, $opt->{coverage} );
-
-            }
-
-            {    # second seq
-                if ( !exists $covered->{ $info->{g_id} } ) {
-                    $covered->{ $info->{g_id} }
-                        = { all => AlignDB::IntSpan->new->add_pair( 1, $info->{g_len} ), };
-                    for my $i ( 1 .. $opt->{coverage} ) {
-                        $covered->{ $info->{g_id} }{$i} = AlignDB::IntSpan->new;
-                    }
-                }
-
-                my ( $beg, $end, ) = App::Anchr::Common::beg_end( $info->{g_B}, $info->{g_E}, );
-                App::Anchr::Common::bump_coverage( $covered->{ $info->{g_id} },
-                    $beg, $end, $opt->{coverage} );
-            }
+        if ( !$tempdir->child("$basename.covered.txt")->is_file ) {
+            Carp::croak "Failed: create $basename.covered.txt\n";
         }
-    }
-
-    {    # Create covered.fasta
-        $tempdir->child("covered.txt")->remove;
-
-        for my $key ( sort keys %{$covered} ) {
-            my @subsets = $covered->{$key}{ $opt->{coverage} }->sets;
-            my ($best_part) = map { $_->[0] }
-                sort { $b->[1] <=> $a->[1] }
-                map { [ $_, $_->count() ] } @subsets;
-
-            $tempdir->child("covered.txt")
-                ->append( sprintf( "%s:%s\n", $key, $best_part->runlist ) );
-        }
-
-        if ( !$tempdir->child("covered.txt")->is_file ) {
-            Carp::croak "Failed: create covered.txt\n";
-        }
-
     }
 
     {    # Outputs. stdout is handeld by faops
         my $cmd;
-        $cmd .= "faops region -l 0 $infile covered.txt $opt->{outfile}";
+        $cmd .= "faops region -l 0 $infile $basename.covered.txt $opt->{outfile}";
         App::Anchr::Common::exec_cmd( $cmd, { verbose => $opt->{verbose}, } );
     }
 
--- t/23-trimlong.t
+++ t/23-trimlong.t
@@ -18,7 +18,7 @@ $result = test_app( 'App::Anchr' => [qw(trimlong t/not_exists)] );
 like( $result->error, qr{doesn't exist}, 'infile not exists' );
 
 $result = test_app( 'App::Anchr' => [qw(trimlong t/1_4.pac.fasta -v -o stdout)] );
-is( ( scalar grep {/^CMD/} grep {/\S/} split( /\n/, $result->stderr ) ), 3, 'stderr line count' );
+is( ( scalar grep {/^CMD/} grep {/\S/} split( /\n/, $result->stderr ) ), 4, 'stderr line count' );
 is( ( scalar grep {/\S/} split( /\n/, $result->stdout ) ), 16, 'line count' );
 like( $result->stdout, qr{pac4745_7148}s, 'original names' );
 unlike( $result->stdout, qr{pac4745_7148:1}s, 'uncovered region' );
