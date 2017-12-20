13e9ccfd8bc0ce6fe92429832de7ff1caebcd7c5
wang-q <wang-q@outlook.com>
2017-12-20 08:07:02 +0800

    Restore min length of kunitigs to 1000

--- lib/App/Anchr/Command/kunitigs.pm
+++ lib/App/Anchr/Command/kunitigs.pm
@@ -14,7 +14,7 @@ sub opt_spec {
         [ 'jf=s',         'jellyfish hash size',                  { default => "auto", }, ],
         [ 'estsize=s',    'estimated genome size',                { default => "auto", }, ],
         [ 'kmer=s',       'kmer size to be used',                 { default => "31", }, ],
-        [ 'min=i',        'minimal length of k-unitigs',          { default => 500, }, ],
+        [ 'min=i',        'minimal length of k-unitigs',          { default => 1000, }, ],
         [ 'merge',        'merge k-unitigs from all kmers', ],
         [ 'parallel|p=i', 'number of threads',                    { default => 8, }, ],
         { show_defaults => 1, }
