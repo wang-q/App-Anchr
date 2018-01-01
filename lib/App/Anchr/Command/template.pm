815abf88dffcdd6fff0bb0f83c818ab2bcaa9925
wang-q <wang-q@outlook.com>
2018-01-02 05:28:47 +0800

    Misc changes

--- lib/App/Anchr/Command/template.pm
+++ lib/App/Anchr/Command/template.pm
@@ -497,9 +497,6 @@ parallel --no-run-if-empty --linebuffer -k -j 1 "
                 fi
             fi
         ) \
-[% IF opt.filter -%]
-        --filter [% opt.filter %] \
-[% END -%]
         -p [% opt.parallel %] \
         -o quorum.sh
     bash quorum.sh
