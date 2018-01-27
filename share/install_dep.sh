f0c33a70447c5a9b46c5283682f9ac3a293f6d82
wang-q <wang-q@outlook.com>
2018-01-27 15:59:27 +0800

    No longer use tally (part from reaper)

--- share/install_dep.sh
+++ share/install_dep.sh
@@ -22,7 +22,7 @@ do
     check_install brewsci/science/${package};
 done
 
-for package in faops jrange jrunlist reaper sparsemem dazz_db@20161112 daligner@20170203;
+for package in faops jrange jrunlist sparsemem dazz_db@20161112 daligner@20170203;
 do
     check_install wang-q/tap/${package};
 done
