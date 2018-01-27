5985f58f26c64053c0a0d5ce649acb2a865187a0
wang-q <wang-q@outlook.com>
2018-01-27 15:23:08 +0800

    Remove duplicated parts from mergereads

--- lib/App/Anchr/Command/mergereads.pm
+++ lib/App/Anchr/Command/mergereads.pm
@@ -11,15 +11,11 @@ use constant abstract => "merge reads with bbtools";
 sub opt_spec {
     return (
         [ "outfile|o=s", "output filename, [stdout] for screen", { default => "mergereads.sh" }, ],
+        [ "prefilter=i", "prefilter=N (1 or 2) for tadpole and bbmerge", ],
+        [ "qual|q=i",     "quality score for 3' end",                  { default => 15 }, ],
         [ "len|l=i",      "filter reads less or equal to this length", { default => 60 }, ],
-        [ "tile",         "with normal Illumina names, do tile based filtering", ],
-        [ "prefilter=i",  "prefilter=N (1 or 2) for tadpole and bbmerge", ],
-        [ "filter=s",     "adapter, phix, artifact",                   { default => "adapter" }, ],
-        [ "trimq=i",      "quality score for 3' end",                  { default => 15 }, ],
-        [ "trimk=i",      "kmer for 5' adapter trimming",              { default => 23 }, ],
-        [ "matchk=i",     "kmer for decontamination",                  { default => 27 }, ],
         [ 'ecphase=s',    'Error-correct phases',                      { default => "1,2,3", }, ],
-        [ "parallel|p=i", "number of threads",                         { default => 8 }, ],
+        [ "parallel|p=i", "number of threads",                         { default => 16 }, ],
         { show_defaults => 1, }
     );
 }
@@ -32,6 +28,7 @@ sub description {
     my $desc;
     $desc .= ucfirst(abstract) . ".\n";
     $desc .= "\tFastq files can be gzipped\n";
+    $desc .= "\tFile1 and file2 are paired; or file1 is interleaved\n";
     return $desc;
 }
 
@@ -48,10 +45,6 @@ sub validate_args {
         if ( !Path::Tiny::path($_)->is_file ) {
             $self->usage_error("The input file [$_] doesn't exist.");
         }
-    }
-
-    if ( $opt->{filter} ) {
-        $opt->{filter} = [ grep {defined} split ",", $opt->{filter} ];
     }
 
     unless ( $opt->{ecphase} =~ /^[\d,]+$/ ) {
